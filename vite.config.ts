dff157d31d4ff4b3cb06d8d4e08e939c4a2911f5
Lucas <devlucasteles@gmail.com>
2025-05-30 11:38:22 -0300

    ci: :construction_worker: add github pages build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/S05/" : "/",
   plugins: [TanStackRouterVite(), react()],
   css: {
     postcss: {
